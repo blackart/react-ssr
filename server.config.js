ef5aa2ac9c85b82ee221cc8242ee8ef4e260b6e6
blackart <blackart.ru@gmail.com>
2019-06-11 13:54:19 +0800

    d.ts for styles

--- server.config.js
+++ server.config.js
@@ -29,6 +29,20 @@ module.exports = function (env, argv) {
                     use: [
                         'file-loader'
                     ]
+                },
+                {
+                    test: /\.scss$/,
+                    include: path.join(__dirname, 'src/components'),
+                    use: [
+                        'style-loader',
+                        {
+                            loader: 'typings-for-css-modules-loader',
+                            options: {
+                                modules: true,
+                                namedExport: true
+                            }
+                        }
+                    ]
                 }
             ]
         }
